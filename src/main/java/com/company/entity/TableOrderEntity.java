7777d9019d09e6282dbb7718ae07d8a6656fbfa3
Muhammad Yusuf <myshukurov@gmail.com>
2024-01-09 17:00:20 +0500

    added TableFoodEntity

--- src/main/java/com/company/entity/TableOrderEntity.java
+++ src/main/java/com/company/entity/TableOrderEntity.java
@@ -1,6 +1,7 @@
 package com.company.entity;
 
 import com.company.entity.base.IntegerBaseEntity;
+import com.company.enums.TableStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,4 +20,7 @@ public class TableOrderEntity extends IntegerBaseEntity {
     @Column(name = "table_type")// how many people can sit
     private Integer tableType = 2; //default
 
+    @Column(name = "table_status")
+    @Enumerated(EnumType.STRING)
+    private TableStatus tableStatus;
 }
