ff6189b8793225d631b499bfd3aab56eb279b6a4
Muhammad Yusuf <myshukurov@gmail.com>
2024-01-09 11:05:02 +0500

    first commit

---
+++ src/main/java/com/company/RestaurantOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantOrderingSystemApplication.class, args);
+    }
+
+}
